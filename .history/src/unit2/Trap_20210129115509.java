eb9406ee1664ef7562461087624b55bfbe81b99c
yashg4509 <bludgermoonstone66@gmail.com>
2021-01-29 16:27:35 -0800

    (IP) Unit 2

---
+++ .history/src/unit2/Trap_20210129115509.java
@@ -0,0 +1,10 @@
+package unit2;
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+        int area = ((base1 + base2) * height)/2;
+		return area;
+	}
+}
---
+++ src/unit2/TrapRunner.java
@@ -0,0 +1,15 @@
+package unit2;
+
+public class TrapRunner
+{
+	public static void main( String[] args )
+	{
+		System.out.println( Trap.area( 3, 3, 3) );
+		System.out.println( Trap.area( 5, 6, 7) );
+		System.out.println( Trap.area( 7, 10, 6) );
+		System.out.println( Trap.area( 13, 9, 3) );
+		System.out.println( Trap.area( 6, 11, 4) );
+		System.out.println( Trap.area( 11, 8, 5) );
+	}
+}
+
