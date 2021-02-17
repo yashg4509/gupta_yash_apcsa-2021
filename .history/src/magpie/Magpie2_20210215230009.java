c5f3aa2a9370218e45ecf1005b29c3abba3f23fd
yashg4509 <bludgermoonstone66@gmail.com>
2021-02-16 19:30:13 -0800

    magpie

---
+++ .history/src/magpie/Magpie2_20210215230009.java
@@ -0,0 +1,66 @@
+package magpie;
+
+public class Magpie2
+{
+
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+	
+
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.indexOf("no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		} else if (statement.contains("dog") || statement.contains("cat")) {
+			response = "Tell me more about your pets.";
+		} else if (statement.contains("mauro")) {
+			response = "What a great teacher!";
+		}
+		else if (statement.trim().length() == 0) {
+			response = "Say something, please.";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+
+		return response;
+	}
+}
---
+++ .history/src/magpie/StringExplorer_20210216190759.java
@@ -0,0 +1,54 @@
+package magpie;
+
+public class StringExplorer {
+
+	public static void main(String[] args) {
+		String sample = "The quick brown fox jumped over the lazy dog.";
+
+		// Demonstrate the indexOf method.
+		int position = sample.indexOf("quick");
+		System.out.println("sample.indexOf(\"quick\") = " + position);
+
+		int pos = sample.indexOf("slow");
+		if (pos != -1)
+			System.out.println("slow is found at position " + pos);
+		else
+			System.out.println("slow is not found");
+
+		// Demonstrate the toLowerCase method.
+		String lowerCase = sample.toLowerCase();
+		System.out.println("sample.toLowerCase() = " + lowerCase);
+		System.out.println("After toLowerCase(), sample = " + sample);
+
+		// Try other methods here:
+		String the = "Computer Science is the best, the greatest, and the most wonderful subject to study!";
+		String phrase = the.trim().toLowerCase();
+		String goal = "the";
+		int startPos = 0;
+
+		int pos2 = phrase.indexOf(goal, startPos);
+		if (pos2 >= 0) {
+			while (pos2 >= 0) {
+				String before = " ", after = " ";
+				if (pos2 > 0) {
+					before = phrase.substring(pos2 - 1, pos2);
+				}
+
+				if (pos2 + goal.length() < phrase.length() - 1) {
+					after = phrase.substring(pos2 + goal.length(), pos2 + goal.length() + 1);
+				}
+
+				if ((before.compareTo("a") < 0 || before.compareTo("z") > 0)
+						&& (after.compareTo("a") < 0 || after.compareTo("z") > 0)) {
+					System.out.println(pos2);
+				}
+
+				pos2 = phrase.indexOf(goal, pos2 + 1);
+			}
+		} else {
+			System.out.println("-1");
+		}
+
+	}
+
+}
